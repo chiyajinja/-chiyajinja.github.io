46cda20836e2cb4253cb02222ede5c3146e4ad55
chiyajinja <142967626+chiyajinja@users.noreply.github.com>
2023-09-09 01:24:03 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+let currentImageIndex = 0;
+let imagesLoaded = [];
+
+// 画像のファイル名を00001.jpgから00288.jpgまで配列に格納
+const images = Array.from({ length: 288 }, (_, i) => `url("background_images/00${String(i + 1).padStart(3, '0')}.jpg")`);
+
+// 画像のプリロード
+images.forEach(imageUrl => {
+    const img = new Image();
+    img.src = imageUrl.substring(5, imageUrl.length - 2);  // "url()"を取り除く
+    img.onload = () => {
+        imagesLoaded.push(imageUrl);
+    };
+});
+
+function changeBackgroundImage() {
+    const mainElement = document.querySelector('main');
+
+    // プリロードされた画像がある場合のみ、背景画像を変更
+    if (imagesLoaded.length > 0) {
+        const randomIndex = Math.floor(Math.random() * imagesLoaded.length);
+        mainElement.style.backgroundImage = imagesLoaded[randomIndex];
+    }
+}
+
+// 1.5秒毎にchangeBackgroundImage関数を実行
+setInterval(changeBackgroundImage, 1500);
+
+// 画像のファイル名をkuji00001.jpgからkuji00367.jpgまで配列に格納
+const kujiImages = Array.from({ length: 367 }, (_, i) => `url("kuji_images/kuji${String(i + 1).padStart(5, '0')}.jpg")`);
+
+let kujiImagesLoaded = [];
+
+// くじ画像のプリロード
+kujiImages.forEach(imageUrl => {
+    const img = new Image();
+    img.src = imageUrl.substring(5, imageUrl.length - 2);  // "url()"を取り除く
+    img.onload = () => {
+        kujiImagesLoaded.push(imageUrl);
+    };
+});
+
+function changeKujiImage() {
+    const imageDisplayElement = document.getElementById('image-display');
+
+    // プリロードされたくじ画像がある場合のみ、画像を変更
+    if (kujiImagesLoaded.length > 0) {
+        const randomIndex = Math.floor(Math.random() * kujiImagesLoaded.length);
+        imageDisplayElement.style.backgroundImage = kujiImagesLoaded[randomIndex];
+    }
+}
+
+// ボタンがクリックされたときにchangeKujiImage関数を実行
+document.getElementById('change-image-button').addEventListener('click', changeKujiImage);
