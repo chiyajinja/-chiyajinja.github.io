51d760a351ea497a698f84927b98d88aa69b7fa7
chiyajinja <142967626+chiyajinja@users.noreply.github.com>
2023-09-10 20:08:20 +0900

    Create congrats.js

---
+++ congrats.js
@@ -0,0 +1,7 @@
+const imageElement = document.getElementById("congrats-image");
+const switchCountElement = document.getElementById("switch-count");
+const percentageElement = document.getElementById("percentage");
+
+imageElement.style.backgroundImage = 'url("kuji_images/kuji00001.jpg")'; 
+switchCountElement.textContent = "画面切替回数: 50"; 
+percentageElement.textContent = "確率: 13.7%"; 
