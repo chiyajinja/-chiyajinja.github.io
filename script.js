40966707fec7639a5f9933129f008998247c8631
chiyajinja <142967626+chiyajinja@users.noreply.github.com>
2023-09-11 14:50:30 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -75,13 +75,20 @@ function updateAutoButtonText(isRunning) {
 }
 
 function showCongratulations() {
+    // おみくじの結果を表示する前に自動切り替えを停止
+    if (autoInterval) {
+        clearInterval(autoInterval);
+        autoInterval = null;
+        updateAutoButtonText(false);
+    }
+
     const imageUrl = document.getElementById('image-display').style.backgroundImage;
     document.getElementById('kuji-result').style.backgroundImage = imageUrl;
 
-    document.getElementById('switch-count-result').innerText = `おみくじを引いた回数: ${switchCount}回`;
+    document.getElementById('switch-count-result').innerText = `切り替わった画面の回数: ${switchCount}回`;
 
     const percentage = ((switchCount / kujiImages.length) * 100).toFixed(2);
-    document.getElementById('percentage-result').innerText = `今回のおみくじ確率: ${percentage}%`;
+    document.getElementById('percentage-result').innerText = `画像の総枚数に対する割合: ${percentage}%`;
 
     document.getElementById('congratulations-modal').style.display = 'block';
 }
