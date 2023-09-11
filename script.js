e3f179e6ab15fc74d395710e92599cbc79dd091d
chiyajinja <142967626+chiyajinja@users.noreply.github.com>
2023-09-11 23:14:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,68 +11,109 @@ images.forEach(imageUrl => {
 });
 
 function changeBackgroundImage() {
-    const mainElementconst imageFolderPath = "background_images";
-const kujiFolderPath = "kuji_images";
-let imagesLoaded = [];
-let kujiImagesLoaded = [];
-let switchCount = 0;
-let autoInterval = null;
-
-function loadImages(path, total, format, collection) {
-    const images = Array.from({ length: total }, (_, i) => `url("${path}/${String(i + 1).padStart(format, '0')}.jpg")`);
-
-    images.forEach(imageUrl => {
-        const img = new Image();
-        img.src = imageUrl.slice(5, -2);
-        img.onload = () => {
-            collection.push(imageUrl);
-        };
-    });
-}
-
-function changeBackgroundImage() {
     const mainElement = document.querySelector('main');
-    if (imagesLoaded.length) {
-        mainElement.style.backgroundImage = imagesLoaded[Math.floor(Math.random() * imagesLoaded.length)];
+    if (imagesLoaded.length > 0) {
+        const randomIndex = Math.floor(Math.random() * imagesLoaded.length);
+        mainElement.style.backgroundImage = imagesLoaded[randomIndex];
     }
 }
 
+setInterval(changeBackgroundImage, 1500);
+
+const kujiImages = Array.from({ length: 367 }, (_, i) => `url("kuji_images/kuji${String(i + 1).padStart(5, '0')}.jpg")`);
+let kujiImagesLoaded = [];
+
+kujiImages.forEach(imageUrl => {
+    const img = new Image();
+    img.src = imageUrl.substring(5, imageUrl.length - 2);
+    img.onload = () => {
+        kujiImagesLoaded.push(imageUrl);
+    };
+});
+
+let switchCount = 0;
+
 function increaseCount() {
-    document.getElementById("count-display").innerText = ++switchCount;
+    switchCount++;
+    document.getElementById("count-display").innerText = switchCount;
 }
 
 function checkForSpecialKuji(imageUrl) {
-    if (imageUrl.includes("kuji00001.jpg") || imageUrl.includes("kuji00005.jpg")) {
-        showCongratulations();
+    if (imageUrl.includes("kuji00001.jpg") ||
+        imageUrl.includes("kuji00002.jpg") ||
+        imageUrl.includes("kuji00003.jpg") ||
+        imageUrl.includes("kuji00004.jpg") ||
+        imageUrl.includes("kuji00005.jpg")) {
+            
+            console.log("特定のおみくじ画像を検出!"); // このログを追加
+            
+            showCongratulations();
     }
 }
 
 function displayRandomKujiImage() {
     const imageDisplayElement = document.getElementById('image-display');
-    if (kujiImagesLoaded.length) {
-        const selectedImageUrl = kujiImagesLoaded[Math.floor(Math.random() * kujiImagesLoaded.length)];
+    if (kujiImagesLoaded.length > 0) {
+        const randomIndex = Math.floor(Math.random() * kujiImagesLoaded.length);
+        const selectedImageUrl = kujiImagesLoaded[randomIndex];
         imageDisplayElement.style.backgroundImage = selectedImageUrl;
         increaseCount();
+
+        // ここで特定のおみくじ画像をチェック
         checkForSpecialKuji(selectedImageUrl);
     }
 }
 
+document.getElementById('manual-button').addEventListener('click', displayRandomKujiImage);
+
+let autoInterval = null;
+
 function updateAutoButtonText(isRunning) {
-    document.getElementById("auto-button").innerHTML = isRunning ? "停止" : "オススメ！<br>自動 de<br>おみくじ";
+    const autoButton = document.getElementById("auto-button");
+    if (isRunning) {
+        autoButton.innerHTML = "一旦停止<br>";
+    } else {
+        autoButton.innerHTML = "オススメ！<br>自動 de<br>おみくじ";
+    }
 }
 
 function showCongratulations() {
+    // おみくじの結果を表示する前に自動切り替えを停止
     if (autoInterval) {
         clearInterval(autoInterval);
         autoInterval = null;
         updateAutoButtonText(false);
     }
-    document.getElementById('kuji-result').style.backgroundImage = document.getElementById('image-display').style.backgroundImage;
+    
+    console.log("showCongratulations関数が呼び出されました"); // このログを追加
+
+    const imageUrl = document.getElementById('image-display').style.backgroundImage;
+    document.getElementById('kuji-result').style.backgroundImage = imageUrl;
+
     document.getElementById('switch-count-result').innerText = `おみくじを引いた回数: ${switchCount}回`;
+
     const percentage = ((switchCount / kujiImages.length) * 100).toFixed(2);
     document.getElementById('percentage-result').innerText = `画像の総枚数に対する割合: ${percentage}%`;
+
     document.getElementById('congratulations-modal').style.display = 'block';
 }
+
+// document.body.addEventListener('click', function() {
+//    if (document.getElementById('congratulations-modal').style.display === 'block') {
+//        document.getElementById('congratulations-modal').style.display = 'none';
+//    }
+//});
+
+document.getElementById('congratulations-modal').addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+document.body.addEventListener('click', function() {
+    if (document.getElementById('congratulations-modal').style.display === 'block') {
+        document.getElementById('congratulations-modal').style.display = 'none';
+    }
+});
+
 
 function startAutoKuji() {
     if (autoInterval) {
@@ -80,19 +121,12 @@ function startAutoKuji() {
         autoInterval = null;
         updateAutoButtonText(false);
     } else {
-        autoInterval = setInterval(displayRandomKujiImage, 500);
+        autoInterval = setInterval(() => {
+            displayRandomKujiImage();
+        }, 500);
         updateAutoButtonText(true);
     }
 }
 
-loadImages(imageFolderPath, 288, 3, imagesLoaded);
-loadImages(kujiFolderPath, 367, 5, kujiImagesLoaded);
-setInterval(changeBackgroundImage, 1500);
-document.getElementById('manual-button').addEventListener('click', displayRandomKujiImage);
-document.getElementById('congratulations-modal').addEventListener('click', e => e.stopPropagation());
-document.body.addEventListener('click', () => {
-    if (document.getElementById('congratulations-modal').style.display === 'block') {
-        document.getElementById('congratulations-modal').style.display = 'none';
-    }
-});
 document.getElementById("auto-button").addEventListener("click", startAutoKuji);
+
