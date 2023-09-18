da0e5dd8cc948738a331473eaeb14ae76a466941
chiyajinja <142967626+chiyajinja@users.noreply.github.com>
2023-09-19 02:43:58 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ function hideControls() {
     document.querySelector(".button-container").style.display = "none";
 }
 
+function playSpecialKujiSound() {
+    const sound = new Audio('se/metronome00001.mp3');
+    sound.volume = 0.5; // ボリュームを50%に設定 (0.0 〜 1.0 の範囲で指定)
+    sound.play();
+}
+
 function checkForSpecialKuji(imageUrl) {
     if (imageUrl.includes("kuji00001.jpg") ||
         imageUrl.includes("kuji00002.jpg") ||
@@ -27,9 +33,10 @@ function checkForSpecialKuji(imageUrl) {
         imageUrl.includes("kuji00004.jpg") ||
         imageUrl.includes("kuji00005.jpg")) {
             
-            console.log("特定のおみくじ画像を検出!"); // このログを追加
-            hideControls();  // この行を追加
+            console.log("特定のおみくじ画像を検出!");
+            hideControls();
             showCongratulations();
+            playSpecialKujiSound();  // ここで効果音を再生
     }
 }
 
@@ -61,7 +68,7 @@ let autoInterval = null;
 function updateAutoButtonText(isRunning) {
     const autoButton = document.getElementById("auto-button");
     if (isRunning) {
-        autoButton.innerHTML = "ちょっと<br>とまって";
+        autoButton.innerHTML = "一旦停止";
     } else {
         autoButton.innerHTML = "オススメ<br>自動 de<br>おみくじ";
     }
