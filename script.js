765f1ed4236e5b4eea7ae147e104e96f0db822fb
chiyajinja <142967626+chiyajinja@users.noreply.github.com>
2023-10-27 23:37:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const kujiImages = Array.from({ length: 429 }, (_, i) => `url("kuji_images/kuji${String(i + 1).padStart(5, '0')}.jpg")`);
+const kujiImages = Array.from({ length: 439 }, (_, i) => `url("kuji_images/kuji${String(i + 1).padStart(5, '0')}.jpg")`);
 let kujiImagesLoaded = [];
 
 kujiImages.forEach(imageUrl => {
@@ -81,29 +81,32 @@ document.getElementById('manual-button').addEventListener('click', function(even
     event.stopPropagation();  // この行を追加
     displayRandomKujiImage();
 
+/* 本ページではこの行を削除（1/6）*/
 // 手動のおみくじボタンのクリックをトラッキング
-    gtag('event', 'click Manual Button', {
-        'event_category': 'Omikuji',
-        'event_label': 'Manual Button Click'
+    gtag('event', 'おみくじ手動click数', {
+        'event_category': 'おみくじページ',
+        'event_label': 'おみくじ手動ボタンClick数'
     });
-
+/* 本ページではこの行を削除（2/6）*/
 });
 
+
+/* 本ページではこの行を削除（3/6）*/
 document.getElementById('auto-button').addEventListener('click', function(event) {
     // 自動のおみくじボタンのクリックをトラッキング
-    gtag('event', 'click Auto Button', {
-        'event_category': 'Omikuji',
-        'event_label': 'Auto Button Click'
+    gtag('event', 'おみくじ自動click数', {
+        'event_category': 'おみくじページ',
+        'event_label': 'おみくじ自動ボタンClick数'
     });
 });
-
+/* 本ページではこの行を削除（4/6）*/
 
 let autoInterval = null;
 
 function updateAutoButtonText(isRunning) {
     const autoButton = document.getElementById("auto-button");
     if (isRunning) {
-        autoButton.innerHTML = "一時停止";
+        autoButton.innerHTML = "停止";
     } else {
         autoButton.innerHTML = "オススメ<br>自動 de<br>おみくじ";
     }
@@ -120,12 +123,13 @@ function showCongratulations() {
     
     console.log("showCongratulations関数が呼び出されました"); // このログを追加
 
+/* 本ページではこの行を削除（5/6）*/
     // Google アナリティクスのイベントトラッキング
-    gtag('event', 'view Congrats Modal', {
-        'event_category': 'Modal',
-        'event_label': 'Congrats Modal'
+    gtag('event', 'おみくじ結果ページview', {
+        'event_category': 'おみくじ結果',
+        'event_label': 'おみくじ結果ページview数'
     });
-
+/* 本ページではこの行を削除（6/6）*/
 
     const imageUrl = document.getElementById('image-display').style.backgroundImage;
     // document.getElementById('kuji-result').style.backgroundImage = imageUrl;
